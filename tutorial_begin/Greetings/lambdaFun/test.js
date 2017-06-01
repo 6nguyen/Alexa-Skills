634a25c9ee1c5c320b30bfdb634584c6aa82285c
George Nguyen <g.nguyen92@yahoo.com>
2017-05-31 18:24:09 -0700

    Update test.js validCard function that checks if cards are valid

--- tutorial_begin/Greetings/lambdaFun/test.js
+++ tutorial_begin/Greetings/lambdaFun/test.js
@@ -1,7 +1,9 @@
 'use strict'
 
+// tests object/value properties and presents it in human friendly UI
 var expect = require('chai').expect,  
 
+// testing the file containing handling functions, index.js
 lambdaToTest = require('./index')
 
 
@@ -9,11 +11,13 @@ function Context() {
   this.speechResponse = null;
   this.speechError = null;
 
+  // loading the speechResponse from all succeed calls into rsp
   this.succeed = function(rsp) {
     this.speechResponse = rsp;
     this.done();
   };
 
+  // capturing error message and putting it into rsp
   this.fail = function(rsp) {
     this.speechError = rsp;
     this.done();
@@ -21,11 +25,14 @@ function Context() {
 
 }
 
+// Template checks if responses are valid
 function validRsp(ctx,options) {
      expect(ctx.speechError).to.be.null;
      expect(ctx.speechResponse.version).to.be.equal('1.0');
      expect(ctx.speechResponse.response).not.to.be.undefined;
      expect(ctx.speechResponse.response.outputSpeech).not.to.be.undefined;
+     // .to.be.equal('Plain Text'); if Alexa Skill uses plain text output speech type instead of SSML
+     // next two lines should be changed accordingly to output speech type
      expect(ctx.speechResponse.response.outputSpeech.type).to.be.equal('SSML');
      expect(ctx.speechResponse.response.outputSpeech.ssml).not.to.be.undefined;
      expect(ctx.speechResponse.response.outputSpeech.ssml).to.match(/<speak>.*<\/speak>/);
@@ -41,15 +48,24 @@ function validRsp(ctx,options) {
 
 }
 
-function validCard(ctx) {
+// Template checks if cards are valid
+function validCard(ctx, standardCard) {
      expect(ctx.speechResponse.response.card).not.to.be.undefined;
-     expect(ctx.speechResponse.response.card.type).to.be.equal('Simple');
      expect(ctx.speechResponse.response.card.title).not.to.be.undefined;
-     expect(ctx.speechResponse.response.card.content).not.to.be.undefined;
+     if (standardCard) {
+        expect(ctx.speechResponse.response.card.type).to.be.equal('Standard');
+        expect(ctx.speechResponse.response.card.text).not.to.be.undefined;
+        expect(ctx.speechResponse.response.card.image).not.to.be.undefined;
+        expect(ctx.speechResponse.response.card.image.largeImageUrl).to.match(/^https:\/\//);
+        expect(ctx.speechResponse.response.card.image.smallImageUrl).to.match(/^https:\/\//);
+      } else {
+        expect(ctx.speechResponse.response.card.type).to.be.equal('Simple');
+        expect(ctx.speechResponse.response.card.content).not.to.be.undefined;
+      }
 }
 
 
-
+// TEMPLATE:  replace 'SlotName', 'slot value', 'intent name' and intent 'type'
 var event = {
   session: {
     new: false,
